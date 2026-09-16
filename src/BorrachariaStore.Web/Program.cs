8fccffb1de04f60dab13a591040c1b8bef22ce45
Elisangela Prins <elisangela.prinz@gmail.com>
2026-09-16 19:14:36 -0300

    feat: registra HttpClientFactory no container de injecao de dependencia

--- src/BorrachariaStore.Web/Program.cs
+++ src/BorrachariaStore.Web/Program.cs
@@ -3,14 +3,13 @@ using BorrachariaStore.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Razor Pages (Aula 2)
 builder.Services.AddRazorPages();
 
-// Configuração do MongoDB (Aula 3)
+builder.Services.AddHttpClient();
+
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDbSettings"));
 
-// Serviços de domínio
 builder.Services.AddSingleton<ProdutoService>();
 builder.Services.AddSingleton<CarrinhoService>();
 builder.Services.AddSingleton<PedidoService>();
